daf35cb9874725cdba9af98ea636dd993af7189c
rebeccania <rebeccaniaga.napitupulu@ui.ac.id>
2026-08-31 22:13:46 +0700

    Add STATIC_ROOT configuration

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
