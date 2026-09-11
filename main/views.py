6c0eaa2e8d9461a727d0214fe10479dd52623c5a
rebeccania <rebeccaniaga.napitupulu@ui.ac.id>
2026-09-11 20:20:55 +0700

    feat: add show_education view

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from main.models import Experience
+from main.models import Education
 
 # Create your views here.
 def show_main(request):
@@ -23,3 +24,10 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_education(request):
+    education_list = Education.objects.all().order_by('-start_year')
+    context = {
+        'education_list': education_list,
+    }
+    return render(request, 'education.html', context)
