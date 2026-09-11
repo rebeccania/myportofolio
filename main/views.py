069dc751bea6dedd5cfb1df4522aa9f0f8ac3ca9
rebeccania <rebeccaniaga.napitupulu@ui.ac.id>
2026-09-11 21:43:45 +0700

    fix: pass name context to education & experience view

--- main/views.py
+++ main/views.py
@@ -20,7 +20,7 @@ def show_main(request):
 
 def show_experience(request):
     context = {
-        "name": "Rebecca",
+        "name": "Rebeccaniaga Napitupulu",
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
@@ -28,6 +28,7 @@ def show_experience(request):
 def show_education(request):
     education_list = Education.objects.all().order_by('-start_year')
     context = {
+        'name': 'Rebeccaniaga Napitupulu',
         'education_list': education_list,
     }
     return render(request, 'education.html', context)
