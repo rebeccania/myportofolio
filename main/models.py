95ecbd92ffa920f4658a1de6f7b9a413d36e34a1
rebeccania <rebeccaniaga.napitupulu@ui.ac.id>
2026-09-26 22:25:02 +0700

    add project star/unstar feature with ManyToMany relationship and update API serialization

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 import uuid
+from django.contrib.auth.models import User 
 from django.db import models
 
 # Create your models here.
@@ -66,6 +67,7 @@ class Project(models.Model):
     tech_stack = models.CharField(max_length=255)
     project_url = models.URLField(blank=True)
     project_image_url = models.URLField(blank=True, max_length=500)
+    starred_by = models.ManyToManyField(User, related_name="starred_projects", blank=True)
 
     def __str__(self):
         return self.title
